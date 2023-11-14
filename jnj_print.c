1f3c300bb965a11c2e6c2be9071a936fcab143ac
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2023-11-14 15:50:25 +0300

    Update jnj_print.c

--- jnj_print.c
+++ jnj_print.c
@@ -1,5 +1,10 @@
 #include "shell.h"
 
+/**
+ * jnj_print - it prints a string to standard output.
+ * @jnjstring: the string to be printed.
+ */
+
 void jnj_print(const char *jnjstring)
 {
         write(STDOUT_FILENO, jnjstring, strlen(jnjstring));
