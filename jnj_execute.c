59b80411d2ecfafd1b1b0113b74473a8a7417d4f
jecinta254 <njokijecinta254.com>
2023-11-17 16:01:08 -0500

    shell commits

--- jnj_execute.c
+++ jnj_execute.c
@@ -8,6 +8,7 @@ int status;
 int arg_ument_count = 0;
 char *delm = " ";
 pid_t mtoto_pid;
+
 token = strtok((char *)watho, delm);
 argument_s = malloc(sizeof(char *) * 2);
 
@@ -16,27 +17,42 @@ while (token)
 argument_s[arg_ument_count] = strdup(token);
 token = strtok(NULL, delm);
 arg_ument_count++;
+
 argument_s = realloc(argument_s, sizeof(char *) * (arg_ument_count + 1));
 }
+
 argument_s[arg_ument_count] = NULL;
+
 mtoto_pid = fork();
 
 if (mtoto_pid == -1)
 {
 perror("Fork failed");
-exit(EXIT_FAILURE); }
+exit(EXIT_FAILURE);
+}
 else if (mtoto_pid == 0)
 {
 if (execvp(argument_s[0], argument_s) == -1)
-{ perror("execvp error");
-exit(EXIT_FAILURE); }
+{
+perror("Error in execvp");
+exit(EXIT_FAILURE);
+}
 exit(EXIT_SUCCESS);
 }
 else
 {
 waitpid(mtoto_pid, &status, 0);
 if (WIFEXITED(status))
-{ int exit_status = WEXITSTATUS(status); }
+{
+int exit_status = WEXITSTATUS(status);
+printf("Command '%s' exited with status %d\n", watho, exit_status);
+}
 else if (WIFSIGNALED(status))
-{ int signal_number = WTERMSIG(status); } }
-free(argument_s); }
+{
+int signal_number = WTERMSIG(status);
+printf("Command '%s' terminated by signal %d\n", watho, signal_number);
+}
+}
+
+free(argument_s);
+}
